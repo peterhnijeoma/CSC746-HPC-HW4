14870b9b4dbf8e7301c1ff8081a66acfd1b8dc48
peterhnijeoma <ijeomapeter55@gmail.com>
2021-10-01 22:20:50 -0700

    initial drop

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
-#include <omp.h>
-#include "likwid-stuff.h"
-
-const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
-
 /*
  * This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are n-by-n matrices stored in column-major format.
  * On exit, A and B maintain their input values.
  */
+
+#include <iostream>
+#include <omp.h>
+#include "likwid-stuff.h"
+
+const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
+
 void square_dgemm(int n, double* A, double* B, double* C) 
 {
    // insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
@@ -18,5 +19,20 @@ void square_dgemm(int n, double* A, double* B, double* C)
    // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
    // after the matrix multiply code but before the end of the parallel code block.
 
-   std::cout << "Insert your basic matrix multiply, openmp-parallel edition here " << std::endl;
+   #pragma omp parallel
+   {
+      LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
+      #pragma omp for
+      for (int arow = 0; arow < n; arow++)
+      {
+         for (int bcol = 0; bcol < n; bcol++)
+         {
+            for (int k = 0; k < n; k++)
+            {
+               C[bcol * n + arow] += A[arow + k * n] * B[bcol * n + k];
+            }
+         }
+      } // end #pragma omp for
+      LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+   } // end #pragma omp parallel
 }
--- dgemm-blocked-omp.cpp
+++ dgemm-blocked-omp.cpp
@@ -1,3 +1,9 @@
+/* This routine performs a dgemm operation
+ *  C := C + A * B
+ * where A, B, and C are n-by-n matrices stored in column-major format.
+ * On exit, A and B maintain their input values.
+ * */
+
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
@@ -6,11 +12,42 @@
 
 const char* dgemm_desc = "Blocked dgemm, OpenMP-enabled";
 
+void copy_matrix_block(double **S, double **D, int brl, int bcl, int bs)
+{
+  for (int row = brl; row < bs; row++)
+  {
+     for (int col = bcl; col < bs; col++)
+     {
+        D[row][col] = S[row][col];
+     }
+  }
+}
 
-/* This routine performs a dgemm operation
- *  C := C + A * B
- * where A, B, and C are n-by-n matrices stored in column-major format.
- * On exit, A and B maintain their input values. */
+void matrix_multiply(double **AS, double **BS, double **PROD, int num_rows, int num_cols)
+{
+   for (int row = 0; row < num_rows; row++)
+   {
+      for (int col = 0; col < num_cols; col++)
+      {
+         for (int k = 0; k < num_cols; k++)
+         {
+            PROD[row][col] += AS[row][k] * BS[k][col];
+         }
+      }
+   }
+}
+
+void copy_block_to_matrix(double **S, double **D, int brl, int bcl, int bs)
+{
+  for (int row = 0; row < bs; row++)
+  {
+     for (int col = 0; col < bs; col++)
+     {
+        D[brl+row][bcl+col] = S[row][col];
+     }
+  }
+}
+
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
    // insert your code here: implementation of blocked matrix multiply with copy optimization and OpenMP parallelism enabled
@@ -19,5 +56,101 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
    // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
    // after the matrix multiply code but before the end of the parallel code block.
 
-   std::cout << "Insert your blocked matrix multiply with copy optimization, openmp-parallel edition here " << std::endl;
+  int ii, kk, jj;     // block row and col indexes
+ 
+  // declare and dynamically allocate 2D arrays
+  double **AA, **BB, **CC;
+  double **AAA, **BBB, **CCC;
+
+  AA = new double *[n];
+  BB = new double *[n];
+  CC = new double *[n];
+  for (int i = 0; i < n; i++)
+  {
+     AA[i] = new double [n];
+     BB[i] = new double [n];
+     CC[i] = new double [n];
+  }
+
+  // allocate memory for block matrix copy
+
+  AAA = new double *[block_size];
+  BBB = new double *[block_size];
+  CCC = new double *[block_size];
+  for (int i = 0; i < block_size; i++)
+  {
+     AAA[i] = new double [block_size];
+     BBB[i] = new double [block_size];
+     CCC[i] = new double [block_size];
+  }
+  
+  // copy column major vector A, B, and C into 2D arrays AA, BB, and CC respectively 
+  for (int k = 0, i = 0; i < n*n; k++, i+=n)
+  {
+     for (int j = 0; j < n; j++)
+     {
+        AA[j][k] = A[i+j];
+        BB[j][k] = B[i+j];
+        CC[j][k] = C[i+j];
+     }
+  }
+  
+  // block matrix multiplication logic
+
+  #pragma omp parallel
+  {
+#ifdef LIKWID_PERFMON
+      LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
+#endif
+      #pragma omp for
+      for (ii = 0; ii < n; ii += block_size)  // partition rows by block size; iterate for n/block_size blocks
+      {
+        for (jj = 0; jj < n; jj += block_size) // partition columns by block size; iterate for n/block_size blocks
+        {
+          copy_matrix_block(CC, CCC, ii*block_size, jj*block_size, block_size);
+          for (kk = 0; kk < n; kk += block_size)  // for each row and column of blocks
+          {
+            copy_matrix_block(AA, AAA, ii*block_size, kk*block_size, block_size);
+            copy_matrix_block(BB, BBB, kk*block_size, jj*block_size, block_size);
+            // basic matrix multiple applied to matrix blocks
+            matrix_multiply(AAA, BBB, CCC, block_size, block_size);
+          }
+          // copy block product to produc matrix
+          copy_block_to_matrix(CCC, CC, ii*block_size, jj*block_size, block_size);
+        }
+      } //end #pragma omp for
+#ifdef LIKWID_PERFMON
+      LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+#endif
+  } // end #pragma omp parallel
+  
+  // copy 2d array CC to column major vector C
+  for (int i = 0; i < n; i++)
+  {
+     for (int j = 0; j < n; j++)
+     {
+        C[i*n+j] = CC[j][i];
+     }
+  }
+
+  // release allocated memory
+  for (int i = 0; i < n; i++)
+  {
+     delete [] AA[i];
+     delete [] BB[i];
+     delete [] CC[i];
+  }
+  delete [] AA;
+  delete [] BB;
+  delete [] CC;
+
+  for (int i = 0; i < block_size; i++)
+  {
+     delete [] AAA[i];
+     delete [] BBB[i];
+     delete [] CCC[i];
+  }
+  delete [] AAA;
+  delete [] BBB;
+  delete [] CCC;
 }
