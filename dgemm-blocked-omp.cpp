c69833711cb97202b93c9b6cd6f616ef47a54a5c
peterhnijeoma <ijeomapeter55@gmail.com>
2021-10-04 17:23:20 -0700

    likwid debug

--- dgemm-blocked-omp.cpp
+++ dgemm-blocked-omp.cpp
@@ -118,18 +118,18 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
         for (int jj = 0; jj < n; jj += block_size) // partition columns by block size; iterate for n/block_size blocks
         {
            std::cout << " Thread number is: " << omp_get_thread_num() << "will copy matrix block of CC to CCC for row: " << ii << "; and column: " << jj << "===\n";
-          //copy_matrix_block(CC, CCC, ii*block_size, jj*block_size, block_size);
+          copy_matrix_block(CC, CCC, ii*block_size, jj*block_size, block_size);
           for (int kk = 0; kk < n; kk += block_size)  // for each row and column of blocks
           {
-            //copy_matrix_block(AA, AAA, ii*block_size, kk*block_size, block_size);
-            //copy_matrix_block(BB, BBB, kk*block_size, jj*block_size, block_size);
+            copy_matrix_block(AA, AAA, ii*block_size, kk*block_size, block_size);
+            copy_matrix_block(BB, BBB, kk*block_size, jj*block_size, block_size);
             // basic matrix multiple applied to matrix blocks
-            //matrix_multiply(AAA, BBB, CCC, block_size, block_size);
+            matrix_multiply(AAA, BBB, CCC, block_size, block_size);
             std::cout << " Thread number is: " << omp_get_thread_num() << "ii is: " << ii << "; jj is: " << jj << "; kk is: " << kk << "===\n";
           }
           std::cout << "\n";
           // copy block product to produc matrix
-          //copy_block_to_matrix(CCC, CC, ii*block_size, jj*block_size, block_size);
+          copy_block_to_matrix(CCC, CC, ii*block_size, jj*block_size, block_size);
         }
       } //end #pragma omp for
       for (int i = 0; i < block_size; i++)
